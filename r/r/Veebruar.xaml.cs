5a48705ee4c1dad581e9bff4e232cb771e4d5611
Opilane TTHK <opilane@tthk.ee>
2020-10-16 09:48:05 +0300

    Add project files.

---
+++ r/r/Veebruar.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace r
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Veebruar : ContentPage
+    {
+        public Veebruar()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ r/r/but.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace r
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class but : ContentPage
+    {
+        public but()
+        {
+            InitializeComponent();
+        }
+    }
+}
